853d3d8343df552083c7d3190024e82ec77616b9
atento <96808973+beansonbread@users.noreply.github.com>
2025-04-16 21:40:00 -0400

    implemented fetch, get, post api

---
+++ script.js
@@ -0,0 +1,71 @@
+document.getElementById("fetch").addEventListener("click", function() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ');
+        } else {
+            return response.json();
+        }
+        })
+        .then(data => {
+            console.log(data);
+            displayData(data);
+        })
+        .catch(error => console.error('Error fetching posts:', error));
+});
+
+document.getElementById("xhr").addEventListener("click", function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            console.log(data);
+            displayData(data);
+        } else {
+            console.error('Error fetching data:', xhr.statusText);
+        }
+    };
+
+    xhr.send();
+});
+
+document.getElementById("postForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const message = document.getElementById("message").value;
+
+    const postData = {
+        title: title,
+        body: message,
+    };
+
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        });
+        if (response.ok) {
+            const data = await response.json();
+            alert('Post created successfully!');
+            console.log(data);
+        } else {
+            throw new Error('Network response was not ok ');
+        }
+    } catch (error) {
+        console.error('Error creating post:', error);
+    }
+
+});
+
+function displayData(data) {
+    const postContainer = document.getElementById('data');
+    postContainer.innerHTML = `
+        <h2>${data.title}</h2>
+        <p>${data.body}</p>
+    `;
+}
+
